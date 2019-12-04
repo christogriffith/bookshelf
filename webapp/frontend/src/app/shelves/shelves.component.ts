1f9171dd2d72fbafce290ee3c67bf9db1f29e99b
chris <christo.griffith@gmail.com>
2019-12-03 19:07:02 -0800

    Stuff I missed

--- webapp/frontend/src/app/shelves/shelves.component.ts
+++ webapp/frontend/src/app/shelves/shelves.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Shelf } from '../shelf';
+
+import SHELVES from '../def-shelves.'
 
 @Component({
   selector: 'app-shelves',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShelvesComponent implements OnInit {
 
+  shelf: Shelf = {
+    id: 1,
+    name: "Left0"
+  };
+
   constructor() { }
 
   ngOnInit() {
   }
+  
+  shelves = SHELVES;
 
 }

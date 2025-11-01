152bf00174d7cc654d42afc530f13c5e5bb557df
chris <christo.griffith@gmail.com>
2025-11-01 07:51:12 -0700

    Fix parameter setting

--- scripts/lights.sh
+++ scripts/lights.sh
@@ -12,20 +12,24 @@ setup()
 		if [ ! -e "$GPIOPATH/gpio${i}" ]; then 
 			echo "Exporting and configuring gpio${i} (out, low)"
 			echo "$i" > "$GPIOPATH/export"
-			echo "out" > "$GPIOPATH/gpio${i}/direction"
-			echo "0" > "$GPIOPATH/gpio${i}/value"
 		fi
 	done
 }
 
 setup
 
+
+for i in 17 27; do
+	echo "out" > "$GPIOPATH/gpio${i}/direction"
+	echo "0" > "$GPIOPATH/gpio${i}/value"
+done
+
 if [ $1 = "on" ]; then
 	for i in 17 27; do
-		echo "1" > "$GPIOPATH/gpio${i}/value"
+		echo "1" | sudo tee "$GPIOPATH/gpio${i}/value" > /dev/null
 	done
 elif [ $1 = "off" ]; then
 	for i in 17 27; do
-		echo "0" > "$GPIOPATH/gpio${i}/value"
+		echo "0" | sudo tee "$GPIOPATH/gpio${i}/value" > /dev/null
 	done
 fi
